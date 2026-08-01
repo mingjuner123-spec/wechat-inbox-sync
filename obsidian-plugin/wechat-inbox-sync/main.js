9e75d498cb99ca547677079746e2a77f00b942f9
mingjuner123-spec <mingjuner123@gmail.com>
2026-08-01 12:07:09 +0800

    chore: release plugin 1.3.75 (#45)

--- obsidian-plugin/wechat-inbox-sync/main.js
+++ obsidian-plugin/wechat-inbox-sync/main.js
@@ -761,7 +761,7 @@ var {
 } = require_record_identity_utils();
 var WECHAT_SESSION_PARTITION = "persist:wechat-inbox-wechat";
 var XIAOHONGSHU_SESSION_PARTITION = "persist:wechat-inbox-sync-xiaohongshu";
-var PLUGIN_RUNTIME_VERSION = "1.3.74";
+var PLUGIN_RUNTIME_VERSION = "1.3.75";
 var PLUGIN_RUNTIME_BUILD_MARKER = "clipboard-link-path-v1";
 var LEGACY_OFFICIAL_SYNC_API_BASES = [
   "https://he02-d8gebzv050ed6c4ef-d350b93bf-1357443479.ap-shanghai.app.tcloudbase.com/sync"
