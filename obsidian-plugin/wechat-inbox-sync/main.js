1332482a3a5ad78bab4153b665dc70a5a2c6d966
mingjuner123-spec <mingjuner123@gmail.com>
2026-08-11 23:20:48 +0800

    Release WeChat Inbox Sync 1.3.80

--- obsidian-plugin/wechat-inbox-sync/main.js
+++ obsidian-plugin/wechat-inbox-sync/main.js
@@ -3354,7 +3354,7 @@ var {
 });
 var WECHAT_SESSION_PARTITION = "persist:wechat-inbox-wechat";
 var XIAOHONGSHU_SESSION_PARTITION = "persist:wechat-inbox-sync-xiaohongshu";
-var PLUGIN_RUNTIME_VERSION = "1.3.79";
+var PLUGIN_RUNTIME_VERSION = "1.3.80";
 var PLUGIN_RUNTIME_BUILD_MARKER = "clipboard-link-path-v1";
 var LEGACY_OFFICIAL_SYNC_API_BASES = [
   "https://he02-d8gebzv050ed6c4ef-d350b93bf-1357443479.ap-shanghai.app.tcloudbase.com/sync"
